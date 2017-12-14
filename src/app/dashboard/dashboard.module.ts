0790409a58ad967ad71b9435ac07ae1ad9503935
Sviatoslav Paliichuk <spaliichuk@spaliichuk2-wf.hw.griddynamics.net>
2017-12-14 17:36:17 +0200

    implement json reading

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { PolyComponent } from '../poly/poly.component';
+import {  HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
-        
+    HttpModule    
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   declarations: [DashboardComponent, PolyComponent]
--- src/app/poly/poly.component.ts
+++ src/app/poly/poly.component.ts
@@ -1,25 +1,52 @@
 import { Component } from '@angular/core';
 import { PolymerElement } from '@vaadin/angular2-polymer';
+import { HttpClientModule } from '@angular/common/http';
+import { Http, Response } from '@angular/http'
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-poly',
   templateUrl: './poly.component.html',
-  styleUrls: ['./poly.component.scss'] 
+  styleUrls: ['./poly.component.scss']
 })
 
 export class PolyComponent {
+  url = 'https://jsonplaceholder.typicode.com/photos';
+  data = {};
+
   title = 'demo works!';
   date = '';
   hostValue = 'User'
+
+  collection: any;
 
   items = [
     'one',
     'two',
     'three'
   ];
-  
+
   ngOnInit() {
     this.date = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
+  }
+
+  constructor(private http: Http) {
+      this.getContacts();
+      this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.url)
+      .map((res: Response) => res.json());
+  }
+
+  getContacts(){
+      this.getData().subscribe(data => {
+          console.log(data);
+          this.data = data;
+      })
+
+      console.log(this.data);
   }
 
   onSelected($event) {
@@ -27,11 +54,11 @@ export class PolyComponent {
     this.date = calendarDate;
   }
 
-  onValueChange($event){
+  onValueChange($event) {
     // TODO :: implement two way binding
   }
 
-  f(){
+  f() {
     console.log('asdasd');
   }
 }
